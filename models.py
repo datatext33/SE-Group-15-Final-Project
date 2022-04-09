118a9c8684f1d988ca575254105b6e884aa87811
datatext33 <98487388+datatext33@users.noreply.github.com>
2022-04-08 22:20:14 -0400

    Linting and code cleanup

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+"""
+This is the database model
+"""
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -13,16 +16,3 @@ class AppUser(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(100))
-    reviews = db.relationship("Review", backref="app_user", lazy=True)
-
-
-class Review(db.Model):
-    """
-    Review model
-    """
-
-    id = db.Column(db.Integer, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    comment = db.Column(db.String(200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("app_user.id"), nullable=False)
